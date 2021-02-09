e98fad0d27ae259273c34b7738f6c6214f1c2801
Ewan Pugh <ewan_pugh@live.co.uk>
2021-02-09 11:05:52 +0000

    Renamed package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
-    packages=['px4_api'],
+    packages=['quadrotor_vision_landing'],
     package_dir={'': 'src'}
 )
 setup(**d)
